e0b74452de950b3877b1a77e0d8bdd3f15eb190a
DanTestehan <tdan89@yahoo.com>
2023-03-23 18:23:38 +0200

    practice

---
+++ src/main/java/com/testehan/leetcode/easy/e26/Solution.java
@@ -0,0 +1,22 @@
+package com.testehan.leetcode.easy.e26;
+
+// 26. Remove Duplicates from Sorted Array
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+
+        int uniqueIndex=1;
+        for (int i=0;i<nums.length-1;i++){
+            if (nums[i]<nums[i+1]){
+                nums[uniqueIndex] = nums[i+1];
+                uniqueIndex++;
+            }
+        }
+
+
+        return uniqueIndex;
+    }
+}
---
+++ src/main/java/com/testehan/leetcode/easy/e27/Solution.java
@@ -0,0 +1,19 @@
+package com.testehan.leetcode.easy.e27;
+
+// 27. Remove Element
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums.length==0){
+            return 0;
+        }
+        int k=0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
