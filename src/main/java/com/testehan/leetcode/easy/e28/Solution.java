c3a4bbcd725e557ed73734e6360bd46cdab53e4c
DanTestehan <tdan89@yahoo.com>
2023-03-23 18:24:17 +0200

    practice

---
+++ src/main/java/com/testehan/leetcode/easy/e28/Solution.java
@@ -0,0 +1,9 @@
+package com.testehan.leetcode.easy.e28;
+
+// 28. Find the Index of the First Occurrence in a String
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
