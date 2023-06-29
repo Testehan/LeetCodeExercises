673cf53ade17dcb131155402040ff9c9ce8d5610
DanTestehan <tdan89@yahoo.com>
2023-06-29 16:44:28 +0300

    practice

---
+++ src/main/java/com/testehan/leetcode/easy/e605/Solution.java
@@ -0,0 +1,33 @@
+package com.testehan.leetcode.easy.e605;
+
+// 605. Can Place Flowers
+
+public class Solution {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+
+        for (int i = 0; i<flowerbed.length;i++){
+            if (flowerbed[i] == 0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1] == 0)){
+                n--;
+                flowerbed[i]=1;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        //int[] flowerbed = {1,0,0,0,1};
+       // int n = 1;        -> true
+
+        int[] flowerbed = {1,0,0,0,1};
+        int n = 2;
+
+        System.out.println(canPlaceFlowers(flowerbed,n));
+    }
+}
