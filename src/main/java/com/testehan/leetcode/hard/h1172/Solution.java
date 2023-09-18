eecca936c2fc8dc6ac8b8fa784eb28d3f5a006de
DanTestehan <tdan89@yahoo.com>
2023-09-18 20:19:19 +0100

    practice

---
+++ src/main/java/com/testehan/leetcode/hard/h1172/Solution.java
@@ -0,0 +1,15 @@
+package com.testehan.leetcode.hard.h1172;
+
+// 1172. Dinner Plate Stacks
+
+public class Solution {
+
+    public static void main(String[] args) {
+        DinnerPlates D = new DinnerPlates(2);  // Initialize with capacity = 2
+        D.push(1);
+        D.push(2);
+        D.push(3);
+        D.push(4);
+        D.push(5);
+    }
+}
